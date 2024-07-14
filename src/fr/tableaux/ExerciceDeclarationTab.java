da00d1f22d9ad1d50a5077c13061446b04b36689
FaizaBK <feiza.mnasri@yahoo.fr>
2024-07-14 17:29:59 +0200

    Third commit, creat new package : fr.tableaux, and add classe :ExerciceDeclarationTab

---
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -0,0 +1,10 @@
+package fr.tableaux;
+
+public class ExerciceDeclarationTab {
+    public static void main(String[] args) {
+    int[] tableau = {8, -7, 12, 1, -2, 14, 17, 9};
+            System.out.println("Élémenet à l'index 0:"+tableau[0]);
+            System.out.println("Longueuer du tableau:"+tableau.length);
+            System.out.println("Élémenet au derner index:"+tableau[tableau.length -1]);
+    }
+}
