88cf210da1dcb4a11d67fe58d5ad8798af300ccf
FaizaBK <feiza.mnasri@yahoo.fr>
2024-07-14 17:35:13 +0200

    Commit num:4, creat new package : fr.algorithme, and add two classes :AffichageIdentite.java and AffichageSuite.java

---
+++ src/fr/algorithme/AffichageIdentite.java
@@ -0,0 +1,10 @@
+package fr.algorithme;
+
+public class AffichageIdentite {
+    public static void main(String[] args) {
+        String identite="Faiza MNASRI";
+        for (int i=0;i<10; i++) {
+            System.out.println("Identité"+(i+1)+":"+identite);
+        }
+    }
+}
