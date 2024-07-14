f58aadf415504080cec9143a54160854fea820c6
FaizaBK <feiza.mnasri@yahoo.fr>
2024-07-14 18:36:55 +0200

    Commit num: 7, update package : fr.tableaux, update classe : ExerciceTableauChaines.java

--- src/fr/tableaux/ExerciceTableauChaines.java
+++ src/fr/tableaux/ExerciceTableauChaines.java
@@ -1,6 +1,25 @@
 package fr.tableaux;
 
 public class ExerciceTableauChaines {
-    // Commentaire
-    // new commentaire
+    public static void main(String[] args) {
+        // Déclaration d'un tableau de chaînes de caractères contenant 5 noms de villes
+        String[] villes = {"Toulouse", "Montpellier", "Paris", "Marseille", "Bordeaux"};
+
+        System.out.println("Villes:");
+        System.out.println(villes[0]);
+        System.out.println(villes[1]);
+        System.out.println(villes[2]);
+        System.out.println(villes[3]);
+        System.out.println(villes[4]);
+        System.out.println("\nLongueur du tableau : " + villes.length);
+
+        villes[3] = "Reims";
+        System.out.println("\nVilles après modification :");
+        System.out.println(villes[0]);
+        System.out.println(villes[1]);
+        System.out.println(villes[2]);
+        System.out.println(villes[3]);
+        System.out.println(villes[4]);
+
+    }
 }
