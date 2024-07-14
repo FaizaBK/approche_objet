f0a7c8f44d6a0f9cf19d2ab6eaabc286d9a6ae83
FaizaBK <feiza.mnasri@yahoo.fr>
2024-07-14 17:16:48 +0200

    Seconde commit, dans package : fr.declaration.variable,ajout  classe : DeclarationApp, et delet old file main.java.

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte x=1;
+        short y=125;
+        int z=130;
+        long xx =10000000000L;
+        float n=0.5f;
+        double m=0.20;
+        char ch='c';
+        boolean b=true;
+        String s="coucou";
+        String randomString = "Voici le résultat d'un calcul:1+5=6";
+
+        System.out.println(randomString);
+
+    }
+}
